4b3b8d668cbda1fc61f573f39732c9ed57089341
Oleg Kireev <okireev@ubic.tech>
2023-08-07 15:14:58 +0400

    Init boilerplate project

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['@ubic/eslint-config/react-typescript'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'normalize.css';
+
+function App() {
+  return (
+    <div>
+      Hello Vite
+    </div>
+  )
+}
+
+export default App
