6a43edb36715b1cacdc02f7e64c1ea7ee91ae4ed
Oleg Kireev <okireev@ubic.tech>
2023-08-08 14:32:44 +0400

    Add LoginForm to App

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import { LoginForm } from '@/features/ui/LoginForm';
+
+export const App = function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
