304621627fdc8d21535efa1738f83d447df0e81e
Oleg Kireev <okireev@ubic.tech>
2023-08-09 14:55:56 +0400

    Add Button loading state

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -5,18 +5,24 @@ import styles from './styles.module.scss';
 interface ButtonProps {
   children: ReactNode;
   isWide?: boolean;
+  isLoading?: boolean;
 }
 
 export const Button = function Button({
   children,
   type = 'button',
   isWide = false,
+  isLoading = false,
   ...rest
 }: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...rest}
-      className={clsx(styles.button, rest.className, { [styles.wide]: isWide })}
+      className={clsx(styles.button, rest.className, {
+        [styles.wide]: isWide,
+        [styles.loading]: isLoading,
+      })}
+      disabled={isLoading || rest.disabled}
       // eslint-disable-next-line react/button-has-type
       type={type}
     >
