6c9686ace8c9ba7b4aedc37e4b9e4bd8a1a775e8
Oleg Kireev <okireev@ubic.tech>
2023-08-10 17:09:29 +0400

    Create MainNavigation

---
+++ src/features/app-navigation/model/constants.ts
@@ -0,0 +1,20 @@
+import { type NavigationLink } from '@/shared';
+
+export const LINKS: NavigationLink[] = [
+  {
+    url: '/',
+    title: 'Home',
+  },
+  {
+    url: '/about',
+    title: 'About',
+  },
+  {
+    url: '/contacts',
+    title: 'Contacts',
+  },
+  {
+    url: '/login',
+    title: 'Login',
+  },
+];
---
+++ src/features/app-navigation/ui/MainNavigation/index.tsx
@@ -0,0 +1,6 @@
+import { LINKS } from '../../model';
+import { Navigation } from '@/shared';
+
+export const MainNavigation = function MainNavigation() {
+  return <Navigation links={LINKS} />;
+};
