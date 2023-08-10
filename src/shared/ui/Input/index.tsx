6e62dc94daab68240b9dca8033db533803b3be4b
Oleg Kireev <okireev@ubic.tech>
2023-08-10 18:46:06 +0400

    Create show password button

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -1,5 +1,11 @@
-import { type InputHTMLAttributes } from 'react';
+import {
+  type MouseEventHandler,
+  type InputHTMLAttributes,
+  useRef,
+  useState,
+} from 'react';
 import { clsx } from 'clsx';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import styles from './styles.module.scss';
 
 interface InputProps {
@@ -10,14 +16,35 @@ export const Input = function Input({
   hasError,
   ...rest
 }: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isPasswordShow, setIsPasswordShow] = useState(false);
+  const isPassword = rest.type === 'password';
+
+  const handleShowPasswordClick: MouseEventHandler<HTMLButtonElement> = () => {
+    setIsPasswordShow((prev) => !prev);
+  };
+
   return (
-    <input
-      {...rest}
-      className={clsx(
-        styles.input,
-        { [styles.error]: hasError },
-        rest.className,
+    <div className={clsx(styles.wrapper, rest.className)}>
+      <input
+        {...rest}
+        className={clsx(styles.input, {
+          [styles.error]: hasError,
+          [styles.rightPadding]: isPassword,
+        })}
+        type={isPasswordShow ? 'text' : rest.type}
+        ref={inputRef}
+      />
+      {isPassword && (
+        <button
+          type="button"
+          className={styles.showPassword}
+          title={`${isPasswordShow ? 'Hide' : 'Show'} password`}
+          onClick={handleShowPasswordClick}
+        >
+          {isPasswordShow ? <EyeOffIcon /> : <EyeIcon />}
+        </button>
       )}
-    />
+    </div>
   );
 };
