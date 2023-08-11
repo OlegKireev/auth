8950d75fa02c85cbfc46fc906b0e64c92ddace04
Oleg Kireev <okireev@ubic.tech>
2023-08-11 10:42:47 +0400

    Add mock login controller

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -6,15 +6,42 @@ export interface LoginServiceParams {
 }
 
 export interface LoginServiceResult {
-  token: string;
+  token: string | null;
 }
 
-export const loginService = async ({
-  username,
-  password,
-}: LoginServiceParams): Promise<LoginServiceResult> => {
-  console.log(username, password);
+const USERS_DB: LoginServiceParams[] = [
+  {
+    username: 'admin',
+    password: 'admin',
+  },
+  {
+    username: 'user',
+    password: 'user',
+  },
+];
+
+const loginController = async (
+  user: LoginServiceParams,
+): Promise<LoginServiceResult> => {
+  const foundUser = USERS_DB.find(
+    (dbUser) => dbUser.username === user.username,
+  );
+  const isPasswordCorrect = foundUser?.password === user.password;
 
   await delay(500);
-  return Promise.resolve({ token: '123' });
+
+  if (!foundUser || !isPasswordCorrect) {
+    return Promise.reject(new Error('Incorrect username or password'));
+  }
+
+  return Promise.resolve({
+    token: Math.random().toString(),
+  });
+};
+
+export const loginService = async (
+  user: LoginServiceParams,
+): Promise<LoginServiceResult> => {
+  const response = await loginController(user);
+  return response;
 };
