1428b3643ccdece09cf42a3d5a12cdfe611091fa
Oleg Kireev <okireev@ubic.tech>
2023-08-18 18:45:06 +0400

    Add UserMenu to MainNavigation

--- src/entities/user/ui/UserMenu/index.tsx
+++ src/entities/user/ui/UserMenu/index.tsx
@@ -1,6 +1,7 @@
+import { LogOutIcon } from 'lucide-react';
 import { type User } from '../../types';
 import styles from './styles.module.scss';
-import { Button } from '@/shared';
+import { Menu, MenuButton, MenuItem, MenuList } from '@/shared';
 
 interface UserMenuProps {
   user: User | null;
@@ -12,9 +13,16 @@ export const UserMenu = function UserMenu({
   onLogoutClick,
 }: UserMenuProps) {
   return (
-    <div className={styles.userMenu}>
-      {user?.email || 'Не вошел'}
-      <Button onClick={onLogoutClick}>Logout</Button>
-    </div>
+    <Menu>
+      <MenuButton>{user?.email || 'Не вошел'}</MenuButton>
+      <MenuList>
+        <MenuItem
+          className={styles.menuItem}
+          onClick={onLogoutClick}
+        >
+          Logout <LogOutIcon size={16} />
+        </MenuItem>
+      </MenuList>
+    </Menu>
   );
 };
