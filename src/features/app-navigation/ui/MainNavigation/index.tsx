32ff5a2d865f2c3b564a2bd5f3ab8046da8136c1
Oleg Kireev <okireev@ubic.tech>
2023-08-18 12:00:36 +0400

    Add UserMenu to MainNavigation

--- src/features/app-navigation/ui/MainNavigation/index.tsx
+++ src/features/app-navigation/ui/MainNavigation/index.tsx
@@ -1,6 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import { LINKS } from '../../model';
+import styles from './styles.module.scss';
+import { UserMenu, useAuthContext } from '@/entities/user';
 import { Navigation } from '@/shared';
 
 export const MainNavigation = function MainNavigation() {
-  return <Navigation links={LINKS} />;
+  const { user, logout } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login');
+  };
+
+  return (
+    <div className={styles.mainNavigation}>
+      <Navigation links={LINKS} />
+      <UserMenu
+        user={user}
+        onLogoutClick={handleLogout}
+      />
+    </div>
+  );
 };
